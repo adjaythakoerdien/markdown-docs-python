e050898811423b01235c16fdddbe316993f70486
adjaythakoerdien <a.thakoerdien@gmail.com>
2024-06-16 20:59:47 +0200

    Improvements

--- main.py
+++ main.py
@@ -11,38 +11,46 @@ templates = Jinja2Templates(directory="templates")
 
 def list_dir():
     links = []
-    for root, dirs, files in os.walk("md/", topdown=True):
-        print(root, dirs, files)
+    for _, _, files in os.walk("md/", topdown=True):
         links = [f for f in files if ".md" in f]
+    links = sorted(links)
     return links
 
 
-def process_markdown(text: str):
+def add_links(text, path):
     links = list_dir()
     html_links = ""
     for link in links:
         link_string = link.replace(".md", "")
-        html_links += f"<a href='{link}'>{link_string}</a> <span style='color: #4ade80;'>|</span> "
+        html_links += f"<a href='{path}{link}'>{link_string}</a> <span style='color: #4ade80;'>|</span> "
     html_links += "<br><hr>\n\n"
     text = html_links + text
+    return text
+
+
+def process_markdown(text: str):
+    html_links = ""
+
     text = markdown.markdown(text, extensions=["fenced_code", "codehilite"])
+    text = text.replace("<a", "<a target='_blank'")
     text = text.replace("<h1>", "<h1><span class='h1'># </span>")
     text = text.replace("<h2>", "<h2><span class='h1'>## </span>")
-    text = text.replace("<h3>", "<h3><span class='h1'>### </span>")
+    # text = text.replace("<h3>", "<h3><span class='h1'>### </span>")
     text = text.replace(
         "<blockquote>", "<div class='square-brackets-quote'><blockquote>"
     )
     text = text.replace("</blockquote>", "</div></blockquote>")
+
     return text
 
 
 @app.get("/view/{filename}")
 async def root(request: Request, filename: str):
-    print("[FILENAME] - ", filename)
     with open(f"md/{filename}", "r", encoding="utf8") as infile:
         text = infile.read()
 
     text = process_markdown(text)
+    text = add_links(text, "")
     return templates.TemplateResponse(
         request=request,
         name="first.html",
@@ -53,22 +61,11 @@ async def root(request: Request, filename: str):
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    body = ""
-    links = list_dir()
-    html_links = ""
-    for link in links:
-        link_string = link.replace(".md", "")
-        html_links += f"<a href='/view/{link}'>{link_string}</a> <span style='color: #4ade80;'>|</span> "
-    html_links += "<br><hr>\n\n"
-    body = html_links + body
-    body = markdown.markdown(body)
-    body = body.replace("<h1>", "<h1><span class='h1'># </span>")
-    body = body.replace("<h2>", "<h2><span class='h2'>## </span>")
-    body = body.replace("<h3>", "<h3><span class='h3'>### </span>")
-    body = body.replace(
-        "<blockquote>", "<div class='square-brackets-quote'><blockquote>"
-    )
-    body = body.replace("</blockquote>", "</div></blockquote>")
+    with open("md/home.md", "r", encoding="utf8") as infile:
+        body = infile.read()
+
+    body = process_markdown(body)
+    body = add_links(body, "/view/")
     return templates.TemplateResponse(
         request=request,
         name="first.html",
---
+++ md/home.md
@@ -0,0 +1,11 @@
+# Markdown files with FastAPI
+
+## Content
+
+All the page content for this website is stored in markdown files and gets rendered with jinja2 in FastAPI.
+
+
+### Honorable mentions  
+
+- [[api.adjay.dev]](https://api.adjay.dev)  
+- [[basic.adjay.dev]](https://basic.adjay.dev)  
