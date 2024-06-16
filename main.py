ff81d92cf9415f6e0580df6796e5fe23372b2a25
adjaythakoerdien <a.thakoerdien@gmail.com>
2024-06-16 08:09:49 +0200

    Minor styling

--- main.py
+++ main.py
@@ -61,6 +61,8 @@ async def read_item(request: Request):
     body = html_links + body
     body = markdown.markdown(body)
     body = body.replace("<h1>", "<h1><span class='h1'># </span>")
+    body = body.replace("<h2>", "<h2><span class='h2'>## </span>")
+    body = body.replace("<h3>", "<h3><span class='h3'>### </span>")
     body = body.replace(
         "<blockquote>", "<div class='square-brackets-quote'><blockquote>"
     )
